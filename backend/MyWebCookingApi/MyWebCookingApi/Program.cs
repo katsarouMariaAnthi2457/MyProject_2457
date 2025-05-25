f48eb99e00a19f95c8169f9d197808976f1d2719
mariakatsa <katsarou.maria.anthi@gmail.com>
2025-05-26 00:09:12 +0300

    Update Program.cs

--- backend/MyWebCookingApi/MyWebCookingApi/Program.cs
+++ backend/MyWebCookingApi/MyWebCookingApi/Program.cs
@@ -58,7 +58,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5175")  
+        policy.WithOrigins("http://localhost:5173")  
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
